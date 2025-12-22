b037d62a6100d3dc245157d0a7c9964856caf5be
Zilfs <rheinlandcast@gmail.com>
2025-12-22 21:10:21 +0700

    feat: config fiber V2

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,30 @@
 package app
 
 import (
+	"context"
+	"log"
 	"manews/config"
 	"manews/lib/auth"
+	"manews/lib/middleware"
+	"manews/lib/pagination"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/rs/zerolog/log"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+
+	"os"
 )
 
 func RunServer() {
 	cfg := config.NewConfig()
 	_, err := cfg.ConnectionPostgres()
 	if err != nil {
-		log.Fatal().Msgf("Failed to connect to database: %v", err)
+		log.Fatal("Failed to connect to database: %v", err)
 		return
 	}
 
@@ -21,4 +33,39 @@ func RunServer() {
 	_ = s3.NewFromConfig(cdfR2)
 
 	_ = auth.NewJwt(cfg)
+	_ = middleware.NewMiddleware(cfg)
+
+	_ = pagination.NewPagination()
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] %{ip} %{status} - %{latency} ${method} ${path}\n",
+	}))
+
+	_ = app.Group("/api")
+
+	go func() {
+		if cfg.App.AppPort == "" {
+			cfg.App.AppPort = os.Getenv("APP_PORT")
+		}
+
+		err := app.Listen(":" + cfg.App.AppPort)
+		if err != nil {
+			log.Fatal("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	signal.Notify(quit, syscall.SIGTERM)
+
+	<-quit
+
+	log.Println("Server shutdown of 5 seconds")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	app.ShutdownWithContext(ctx)
 }
