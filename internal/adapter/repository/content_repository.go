89175d0933469c7009a2c70567f98c47b67519c2
Zilfs <rheinlandcast@gmail.com>
2026-01-30 19:45:22 +0700

    fix (admin api): test api content and fixing preload 2

--- internal/adapter/repository/content_repository.go
+++ internal/adapter/repository/content_repository.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ContentRepository interface {
@@ -86,7 +87,7 @@ func (c *contentRepository) EditContent(ctx context.Context, req entity.ContentE
 func (c *contentRepository) GetContentByID(ctx context.Context, id int64) (*entity.ContentEntity, error) {
 	var modelContent model.Content
 
-	err = c.db.Where("id = ?", id).Preload("User", "Category").First(&modelContent).Error
+	err = c.db.Where("id = ?", id).Preload(clause.Associations).First(&modelContent).Error
 	if err != nil {
 		code = "[REPOSITORY] GetContentByID - 1"
 		log.Errorw(code, err)
@@ -123,7 +124,7 @@ func (c *contentRepository) GetContentByID(ctx context.Context, id int64) (*enti
 func (c *contentRepository) GetContents(ctx context.Context) ([]entity.ContentEntity, error) {
 	var modelContents []model.Content
 
-	err := c.db.Order("created_at DESC").Preload("Category", "User").Find(&modelContents).Error
+	err := c.db.Order("created_at DESC").Preload(clause.Associations).Find(&modelContents).Error
 	if err != nil {
 		code = "[REPOSITORY] GetContents - 1"
 		log.Errorw(code, err)
